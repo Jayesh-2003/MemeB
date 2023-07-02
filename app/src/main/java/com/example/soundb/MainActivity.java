54d889d984b48fb2dc4d4e0103afbec64db660c6
Jayesh <krishjaiswal1507@gmail.com>
2023-07-02 17:07:50 +0530

    Added Static Audio Files

--- app/src/main/java/com/example/soundb/MainActivity.java
+++ app/src/main/java/com/example/soundb/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.soundb;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,12 +23,12 @@ public class MainActivity extends AppCompatActivity {
 
         List<String> nameList= Arrays.asList(getResources().getStringArray(R.array.soundNames));
 
-        SoundObject [] soundItems = {new SoundObject(nameList.get(0),R.raw.atharva_laugh),new SoundObject(nameList.get(1),R.raw.athrva_vedya),new SoundObject(nameList.get(2),R.raw.i_am_in_danger),new SoundObject(nameList.get(3),R.raw.motivation),new SoundObject(nameList.get(4),R.raw.nachiket_fav),new SoundObject(nameList.get(5),R.raw.og_vedya),new SoundObject(nameList.get(6),R.raw.rajat),new SoundObject(nameList.get(7),R.raw.rajat_allharam),new SoundObject(nameList.get(8),R.raw.rajat_vedya),new SoundObject(nameList.get(9),R.raw.sandesh)};
+        SoundObject[] soundItems = {new SoundObject(nameList.get(0), R.raw.atharva_laugh), new SoundObject(nameList.get(1), R.raw.athrva_vedya), new SoundObject(nameList.get(2), R.raw.i_am_in_danger), new SoundObject(nameList.get(3), R.raw.motivation), new SoundObject(nameList.get(4), R.raw.nachiket_fav), new SoundObject(nameList.get(5), R.raw.og_vedya), new SoundObject(nameList.get(6), R.raw.rajat), new SoundObject(nameList.get(7), R.raw.rajat_allharam), new SoundObject(nameList.get(8), R.raw.rajat_vedya), new SoundObject(nameList.get(9), R.raw.sandesh), new SoundObject(nameList.get(10), R.raw.github_knowledge)};
 
         soundList.addAll(Arrays.asList(soundItems));
 
-        SoundView =(RecyclerView) findViewById(R.id.soundboardRecyclerView);
-        SoundLayoutManager = new GridLayoutManager(this,3);
+        SoundView = findViewById(R.id.soundboardRecyclerView);
+        SoundLayoutManager = new GridLayoutManager(this, 3);
         SoundView.setLayoutManager(SoundLayoutManager);
         SoundView.setAdapter(SoundAdapter);
     }
