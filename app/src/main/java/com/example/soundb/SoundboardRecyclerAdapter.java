c4fb836751577357b5c088e673d1bd5bb382d1ad
Jayesh <krishjaiswal1507@gmail.com>
2023-07-02 16:25:07 +0530

    Added Static Audio Files

--- app/src/main/java/com/example/soundb/SoundboardRecyclerAdapter.java
+++ app/src/main/java/com/example/soundb/SoundboardRecyclerAdapter.java
@@ -8,9 +8,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class SoundboardRecyclerAdapter extends RecyclerView.Adapter<SoundboardRecyclerAdapter.SoundboardViewHolder> {
-    private String[] soundObjects;
-    public SoundboardRecyclerAdapter(String[] soundObjects){
+    private ArrayList<SoundObject> soundObjects;
+    public SoundboardRecyclerAdapter(ArrayList<SoundObject> soundObjects){
         this.soundObjects=soundObjects;
     }
     @Override
@@ -21,12 +23,23 @@ public class SoundboardRecyclerAdapter extends RecyclerView.Adapter<SoundboardRe
 
     @Override
     public void onBindViewHolder(@NonNull SoundboardViewHolder holder, int position) {
-        holder.itemTextView.setText(soundObjects[position]);
+        final SoundObject object= soundObjects.get(position);
+        final Integer soundID =object.getItemID();
+        holder.itemTextView.setText(object.getItemName());
+
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EventHandlerClass.startMediaPlayer(v,soundID);
+            }
+        });
+
     }
 
     @Override
     public int getItemCount() {
-        return soundObjects.length;
+        return soundObjects.size();
     }
     public class SoundboardViewHolder extends RecyclerView.ViewHolder{
 
